a57b43bb2ec302b71e7508fbb562f697838a5c5d
DESKTOP-ACVIS4O\Admin <khoocassie@ymail.com>
2018-06-17 18:29:15 +0800

    Added a calendar view + some parts of add event

--- app/src/main/java/com/example/admin/testingv1/ProfileActivity.java
+++ app/src/main/java/com/example/admin/testingv1/ProfileActivity.java
@@ -1,10 +1,12 @@
 package com.example.admin.testingv1;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,6 +17,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private FirebaseAuth firebaseAuth;
     private TextView textViewUserEmail;
     private Button buttonLogout;
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +37,16 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
 
         textViewUserEmail.setText("Welcome " + user.getEmail());
-
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + " " + month + " " + year;
+                Intent intent = new Intent(ProfileActivity.this, addEvent.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
+        });
         buttonLogout.setOnClickListener(this);
     }
 
